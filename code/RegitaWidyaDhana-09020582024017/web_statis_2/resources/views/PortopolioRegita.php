b366af8d668ffacfa3e373037de7602047d4f6eb
regitawidya17 <regitawidya@gmail.com>
2021-10-19 14:16:42 +0700

    tugas2

---
+++ code/RegitaWidyaDhana-09020582024017/web_statis_2/resources/views/PortopolioRegita.php
@@ -0,0 +1,146 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+
+    <style>
+        section {
+            min-height: 450px;
+        }
+    </style>
+
+    <title>Regita Widya Dhana</title>
+</head>
+
+<body class="mt-5">
+    <header class="p-3 bg-#ffb6c1 text-white fixed-top">
+        <div class="container">
+            <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+
+                <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
+                    <li><a href="#profile" class="nav-link px-2 text-white">Profile</a></li>
+                    <li><a href="#about_us" class="nav-link px-2 text-white">About</a></li>
+                    <li><a href="#contact_us" class="nav-link px-2 text-white">Contact</a></li>
+                </ul>
+            </div>
+        </div>
+    </header>
+
+    <main>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+            <path fill="#ffb6c1" fill-opacity="1" d="M0,160L16,144C32,128,64,96,96,90.7C128,85,160,107,192,122.7C224,139,256,149,288,154.7C320,160,352,160,384,138.7C416,117,448,75,480,53.3C512,32,544,32,576,69.3C608,107,640,181,672,192C704,203,736,149,768,117.3C800,85,832,75,864,101.3C896,128,928,192,960,208C992,224,1024,192,1056,170.7C1088,149,1120,139,1152,154.7C1184,171,1216,213,1248,224C1280,235,1312,213,1344,192C1376,171,1408,149,1424,138.7L1440,128L1440,320L1424,320C1408,320,1376,320,1344,320C1312,320,1280,320,1248,320C1216,320,1184,320,1152,320C1120,320,1088,320,1056,320C1024,320,992,320,960,320C928,320,896,320,864,320C832,320,800,320,768,320C736,320,704,320,672,320C640,320,608,320,576,320C544,320,512,320,480,320C448,320,416,320,384,320C352,320,320,320,288,320C256,320,224,320,192,320C160,320,128,320,96,320C64,320,32,320,16,320L0,320Z"></path>
+        </svg>
+        <section id="profile">
+            <div class="jumbotron jumbotron-fluid">
+                <div class="container text-center">
+                    <div class="row mb-4">
+                        <div class="col text-center">
+                            <h2>Profile</h2>
+                        </div>
+                    </div>
+                    <img src="image/foto.jpeg" width="25%" class="rounded-circle img-thumbnail">
+                    <h1 class="display-4">Regita Widya Dhana</h1>
+                    <p class="lead">Email : regitawidya@gmail.com <br />
+                        Instagram : regitawidya <br />
+                    </p>
+                </div>
+            </div>
+
+        </section>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+            <path fill="#ffb6c1" fill-opacity="1" d="M0,160L16,144C32,128,64,96,96,90.7C128,85,160,107,192,122.7C224,139,256,149,288,154.7C320,160,352,160,384,138.7C416,117,448,75,480,53.3C512,32,544,32,576,69.3C608,107,640,181,672,192C704,203,736,149,768,117.3C800,85,832,75,864,101.3C896,128,928,192,960,208C992,224,1024,192,1056,170.7C1088,149,1120,139,1152,154.7C1184,171,1216,213,1248,224C1280,235,1312,213,1344,192C1376,171,1408,149,1424,138.7L1440,128L1440,0L1424,0C1408,0,1376,0,1344,0C1312,0,1280,0,1248,0C1216,0,1184,0,1152,0C1120,0,1088,0,1056,0C1024,0,992,0,960,0C928,0,896,0,864,0C832,0,800,0,768,0C736,0,704,0,672,0C640,0,608,0,576,0C544,0,512,0,480,0C448,0,416,0,384,0C352,0,320,0,288,0C256,0,224,0,192,0C160,0,128,0,96,0C64,0,32,0,16,0L0,0Z"></path>
+        </svg>
+        <section id="about_us" class="py-3 px-3">
+            <div class="container">
+                <div class="row pt-5 mb-5">
+                    <div class="col text-center">
+                        <h2>About</h2>
+                    </div>
+                </div>
+
+                <div class="row justify-content-center">
+                    <div class="col-md-5">
+                        <p>
+                            Perkenalkan Nama Saya Regita Widya Dhana,saya mahasiswa Aktif Universitas Sriwijaya Jurusan Komputerisasi Akuntansi Angkatan 2020. Sekarang saya berusia 19 tahun, lahir di Palembang tanggal 17 Agustus 2002.
+                        </p>
+                    </div>
+                    <div class="col-md-5">
+                        <p>
+                            Saya mempunyai keinginan untuk meneruskan studi saya di Universitas Indonesia Jurusan Akuntansi. Karena cita-cita saya ingin menjadi seorang Akuntan.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section id="contact_us" class="contact mb-5">
+            <div class="container">
+                <div class="row pt-4 mb-4">
+                    <div class="col text-center">
+                        <h2>Contact Us</h2>
+                    </div>
+                </div>
+
+                <div class="row justify-content-center">
+                    <div class="col-lg-4">
+                        <div class="card text-white bg-dark mb-3 text-center">
+                            <div class="card-body">
+                                <h5 class="card-title">Contact Us</h5>
+                            </div>
+                        </div>
+                        <ul class="list-group">
+                            <li class="list-group-item">
+                                <h1>Location</h1>
+                            </li>
+                            <li class="list-group-item">My Address</li>
+                            <li class="list-group-item">Simpang Tanjung Harapan, Jl Karya Sepakat No 61, Kenten</li>
+                            <li class="list-group-item">South Sumatera, Indonesia</li>
+                        </ul>
+                    </div>
+                    <div class="col-lg-6">
+                        <form>
+                            <div class="form-group">
+                                <label for="Nama">Nama
+                                    <input type="text" class="form-control" id="Nama" placeholder="Masukan Nama">
+                                </label>
+                            </div>
+                            <div class="form-group">
+                                <label for="Email">Email</label>
+                                <input type="text" class="form-control" id="Email" placeholder="Masukan Email">
+                            </div>
+                            <div class="form-group">
+                                <label for="No Telepon">No Telepon</label>
+                                <input type="text" class="form-control" id="No Telepon" placeholder="Masukan No Telepon">
+                            </div>
+                            <div class="form-group">
+                                <label for="Pesan">Pesan</label>
+                                <textarea name="Pesan" id="Pesan" class="form-control" placeholder="Masukan Pesan"></textarea>
+                            </div>
+                            <div class="form-group">
+                                <button type="button" class="btn btn-dark">Kirim Pesan!</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+
+            </div>
+        </section>
+
+    </main>
+
+
+    <footer class="bg-dark text-white">
+        <div class="container">
+            <div class="row pt-3">
+                <div class="col text-center">
+                    <p>Copyright 2021.</p>
+                </div>
+            </div>
+        </div>
+    </footer>
